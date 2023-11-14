da77b2644e68850c22896199aaadbd6d976ea3c9
Siyu Liu <siyu.java.web@gmail.com>
2023-11-14 12:31:59 +1100

    creates personal details form component and imports it to employee details page and new employee page

--- src/pages/EmployeePage/EmployeePage.tsx
+++ src/pages/EmployeePage/EmployeePage.tsx
@@ -3,11 +3,7 @@ import { useParams } from "react-router-dom";
 import { Employee } from "../../services/employees-service";
 import { EmployeeInfo } from "../../App";
 import { useForm } from "react-hook-form";
-import InputText from "../../components/Form/InputText/InputText";
-import RadioAndCheckBox from "../../components/Form/RadioAndCheckBox/RadioAndCheckBox";
-import Select from "../../components/Form/Select/Select";
-import months from "../../data/months.json";
-import monthsText from '../../data/months-text.json'
+import PersonalDetailsForm from "../PersonalDetailsForm/PersonalDetailsForm";
 export interface FormData {
   firstName: string;
   middleName: string;
@@ -82,169 +78,12 @@ const EmployeePage = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(formSubmit)}>
-      <div>
-        <h1>Personal Information</h1>
-      </div>
-      <InputText
-        name="firstName"
-        label="First name"
-        id="firstName"
-        register={register}
-        errors={errors}
-      />
-      <InputText
-        name="middleName"
-        label="Middle name (if applicable)"
-        id="middleName"
-        register={register}
-        errors={errors}
-      />
-      <InputText
-        name="lastName"
-        label="Last name"
-        id="lastName"
-        register={register}
-        errors={errors}
-      />
-      <div>Contact details</div>
-      <InputText
-        name="email"
-        label="Email address"
-        id="email"
-        register={register}
-        errors={errors}
-      />
-      <InputText
-        name="phone"
-        label="Mobile number"
-        id="phone"
-        register={register}
-        errors={errors}
-      />
-      <InputText
-        name="address"
-        label="Residential address"
-        id="address"
-        register={register}
-        errors={errors}
-      />
-      <div>Employee status</div>
-      <div>
-        <div>What is contract type?</div>
-        <RadioAndCheckBox
-          name="type"
-          label="Permanent"
-          id="permanent"
-          type="radio"
-          value="permanent"
-          register={register}
-          errors={errors}
-        />
-        <RadioAndCheckBox
-          name="type"
-          label="Contract"
-          id="contract"
-          type="radio"
-          value="contract"
-          register={register}
-          errors={errors}
-        />
-      </div>
-
-      <div>
-        <div>Start date</div>
-        <InputText
-          name="startDate.day"
-          label="Day"
-          id="startDay"
-          register={register}
-          errors={errors}
-        />
-        <Select
-          label="Month"
-          name="startDate.month"
-          values={months}
-          text={monthsText}
-          id="startMonth"
-          register={register}
-          errors={errors}
-        />
-        <InputText
-          name="startDate.year"
-          label="Year"
-          id="startYear"
-          register={register}
-          errors={errors}
-        />
-      </div>
-      <div>
-        <div>Finish date</div>
-        <InputText
-          name="finishDate.day"
-          label="Day"
-          id="finishDay"
-          register={register}
-          errors={errors}
-        />
-        <Select
-          label="Month"
-          name="finishDate.month"
-          values={months}
-          text={monthsText}
-          id="finishMonth"
-          register={register}
-          errors={errors}
-        />
-        <InputText
-          name="finishDate.year"
-          label="Year"
-          id="finishYear"
-          register={register}
-          errors={errors}
-        />
-      </div>
-      <RadioAndCheckBox
-        name="onGoing"
-        label="On going"
-        id="onGoing"
-        type="checkbox"
-        value="true"
-        register={register}
-        errors={errors}
-      />
-      <div>
-        <div>Is this on a full-time or part-time basis?</div>
-        <RadioAndCheckBox
-          name="basis"
-          label="Full-time"
-          id="fulltime"
-          type="radio"
-          value="fulltime"
-          register={register}
-          errors={errors}
-        />
-        <RadioAndCheckBox
-          name="basis"
-          label="Part-time"
-          id="parttime"
-          type="radio"
-          value="parttime"
-          register={register}
-          errors={errors}
-        />
-      </div>
-      <InputText
-        name="hoursPerWeek"
-        label="Hours per week"
-        id="hoursPerWeek"
-        register={register}
-        errors={errors}
-      />
-      <div>
-        <button>Save</button>
-      </div>
-    </form>
+    <PersonalDetailsForm
+      register={register}
+      errors={errors}
+      handleSubmit={handleSubmit}
+      formSubmit={formSubmit}
+    />
   );
 };
 
--- src/pages/NewEmployeePage/NewEmployeePage.tsx
+++ src/pages/NewEmployeePage/NewEmployeePage.tsx
@@ -1,8 +1,55 @@
+import { useForm } from "react-hook-form";
+import PersonalDetailsForm from "../PersonalDetailsForm/PersonalDetailsForm";
+import { useEffect } from "react";
 
 const NewEmployeePage = () => {
-  return (
-    <div>NewEmployeePage</div>
-  )
-}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitSuccessful },
+    reset,
+  } = useForm({
+    defaultValues: {
+      firstName: "",
+      middleName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      address: "",
+      type: "permanent",
+      startDate: {
+        day: "",
+        month: "",
+        year: "",
+      },
+      finishDate: {
+        day: "",
+        month: "",
+        year: "",
+      },
+      onGoing: "false",
+      basis: "fulltime",
+      hoursPerWeek: "",
+    },
+    mode: "all",
+  });
 
-export default NewEmployeePage
+  useEffect(() => {
+    reset();
+  }, [isSubmitSuccessful]);
+
+  const formSubmit = () => {
+    console.log("created new employee!");
+  };
+
+  return (
+    <PersonalDetailsForm
+      register={register}
+      errors={errors}
+      handleSubmit={handleSubmit}
+      formSubmit={formSubmit}
+    />
+  );
+};
+
+export default NewEmployeePage;
